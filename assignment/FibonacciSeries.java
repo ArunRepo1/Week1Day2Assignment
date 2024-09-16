9b812076a602425cd458ce86f4588e704870bf1f
ArunRepo1 <157300046+ArunRepo1@users.noreply.github.com>
2024-09-16 21:30:25 +0530

    Add files via upload

---
+++ assignment/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package week1.assignment;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int num=8;
+		int firstNum=0;
+		int SecondNum=1;
+		for(int i=0;i<num;i++)
+		{
+			System.out.println(firstNum);
+			int nextNum=firstNum+SecondNum;
+			firstNum=SecondNum;
+			SecondNum=nextNum;
+			
+	
+			
+			
+		}
+	}
+
+}
---
+++ assignment/checkPositiveNum.java
@@ -0,0 +1,20 @@
+package week1.assignment;
+
+public class checkPositiveNum {
+
+	public static void main(String[] args) {
+		int num=10;
+		if(num>0)
+		{
+			System.out.println("The given number " +num + " is a positive ");
+		}
+		else
+		{
+			System.out.println("The given number " +num + " is a negative ");
+
+			
+		}
+
+	}
+
+}
